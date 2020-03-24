05b016d142e063ec0ee5f777cd7dbadf9fa7f986
andantonyan <andantonyan@gmail.com>
2020-03-25 01:51:08 +0400

    initial commit

---
+++ android/app/src/main/kotlin/care/washreminder/app/MainActivity.kt
@@ -0,0 +1,12 @@
+package care.washreminder.app
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
